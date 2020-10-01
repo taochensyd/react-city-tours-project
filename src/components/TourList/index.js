54c237cedafafb1da5a4f491f7a0a098d85b60f4
Loading <loading@Loadings-MacBook-Pro.local>
2020-10-01 23:47:09 +1000

    first commit

---
+++ src/components/TourList/index.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import "./tourlist.scss";
+import Tour from "../Tour";
+import { tourData } from "../../tourData";
+
+export default class TourList extends Component {
+  state = {
+    tours: tourData,
+  };
+  removeTour = (id) => {
+    // console.log(id);
+    const { tours } = this.state;
+    const sortedTours = tours.filter((tour) => tour.id !== id);
+    this.setState({
+      tours: sortedTours,
+    });
+  };
+  render() {
+    // console.log(this.state.tours);
+    const { tours } = this.state;
+
+    return (
+      <section className="tourlist">
+        {tours.map((tour) => (
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour}></Tour>
+        ))}
+      </section>
+    );
+  }
+}
